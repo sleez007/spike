2a8990b02907ac6939d75eee9a0aa5db115541d5
Kingsley Etoka <etokakingsley@gmail.com>
2025-11-25 00:33:26 +0100

    flow ase setup

--- src/features/dashboard/dashboard.ts
+++ src/features/dashboard/dashboard.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { DescriptionWrapper } from './partials/description-wrapper/description-wrapper';
 import { RiskSummary } from './partials/risk-summary/risk-summary';
 import { AssetList } from './partials/asset-list/asset-list';
+import { Flow } from './partials/flow/flow';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [DescriptionWrapper, RiskSummary, AssetList],
+  imports: [DescriptionWrapper, RiskSummary, AssetList, Flow],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
---
+++ src/features/dashboard/partials/flow/flow.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+
+@Component({
+  selector: 'app-flow',
+  imports: [AngularSvgIconModule],
+  templateUrl: './flow.html',
+  styleUrl: './flow.scss',
+})
+export class Flow {}
